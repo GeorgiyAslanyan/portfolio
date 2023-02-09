e98e8defa13841b95203caded05a9445e704659d
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-02-09 13:16:55 +0500

    contact mini fix

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import Popup from "./Popup";
 
 const Contact = () => {
   const [name, setName] = React.useState("");
@@ -8,31 +7,58 @@ const Contact = () => {
   const [description, setDescription] = React.useState("");
   const [show, setShow] = React.useState(false);
 
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const data = {
+      name,
+      mail,
+      description,
+    };
+    console.log(data);
+    setName("")
+    setMail("")
+    setDescription("")
+  };
+
   return (
     <div className="z-10 relative" id="contact">
       <h2 className="text-2xl font-semibold p-5">Contact</h2>
       <div className="glassBackground w-[60%]">
-        <div className="flex w-full justify-between mb-4">
-          <input
-            type="text"
-            placeholder="Name:"
-            className="p-3 w-[45%] bg-transparent outline-none border-b border-[#CDCDCD]"
+        <form className="telegram-form" onSubmit={handleSubmit}>
+          <div className="flex w-full justify-between mb-4">
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              name="name"
+              placeholder="Name:"
+              className="p-3 w-[45%] bg-transparent outline-none border-b border-[#CDCDCD]"
+            />
+            <input
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
+              type="text"
+              placeholder="Email:"
+              name="email"
+              className="p-3 w-[50%] bg-transparent outline-none border-b border-[#CDCDCD]"
+            />
+          </div>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            name="text"
+            placeholder="Enter your message"
+            className="p-3 w-full bg-transparent outline-none border border-[#CDCDCD] min-h-[200px]"
           />
-          <input
-            type="text"
-            placeholder="Email:"
-            className="p-3 w-[50%] bg-transparent outline-none border-b border-[#CDCDCD]"
-          />
-        </div>
-        <textarea
-          name=""
-          id=""
-          placeholder="Enter your message"
-          className="p-3 w-full bg-transparent outline-none border border-[#CDCDCD] min-h-[200px]"
-        />
-        <div className="flex justify-end">
-          <button className="border border-[#CDCDCD] px-10 py-1">Send</button>
-        </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="border border-[#CDCDCD] px-10 py-1"
+            >
+              Send
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
