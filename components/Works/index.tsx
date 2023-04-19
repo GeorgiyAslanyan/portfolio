b42e4485bf9b889c471d74739ac0062e6ede1f01
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-04-19 13:13:55 +0500

    add project

--- components/Works/index.tsx
+++ components/Works/index.tsx
@@ -51,19 +51,19 @@ const Works = () => {
           </div>
         </div>
       </div>
-      <div className="glassBackground">
+      <div className="glassBackground mb-4">
         <div className="block md:flex justify-between">
           <div className="gap-y-3 grid md:w-2/3 w-full">
             <h3 className="text-[20px] font-semibold">Social (beta)</h3>
             <div>
               <p className="text-[#5c5c5c]">Description</p>
-              <p className="">
-                Social app
-              </p>
+              <p className="">Social app</p>
             </div>
             <div>
               <p className="text-[#5c5c5c]">Frontend</p>
-              <p className="">Next, tailwind, heroicons, react-loader-spinner</p>
+              <p className="">
+                Next, tailwind, heroicons, react-loader-spinner
+              </p>
             </div>
             <div>
               <p className="text-[#5c5c5c]">Backend</p>
@@ -83,6 +83,44 @@ const Works = () => {
             </a>
             <img
               src="/social.webp"
+              alt="socail"
+              className="lg:block hidden object-cover h-full max-w-[370px] rounded-md"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="glassBackground">
+        <div className="block md:flex justify-between">
+          <div className="gap-y-3 grid md:w-2/3 w-full">
+            <h3 className="text-[20px] font-semibold">Web 3D (beta)</h3>
+            <div>
+              <p className="text-[#5c5c5c]">Description</p>
+              <p className="">Webapp with 3d elements</p>
+            </div>
+            <div>
+              <p className="text-[#5c5c5c]">Frontend</p>
+              <p className="">
+                React three fiber, React, tailwind, react-loader-spinner
+              </p>
+            </div>
+            <div>
+              <p className="text-[#5c5c5c]">Backend</p>
+              <p className="">none</p>
+            </div>
+          </div>
+          <div className="relative mt-4 rounded-md">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://webapp3d.vercel.app/"
+              className="absolute bottom-3 right-3 px-3 py-1 bg-[#000000] flex rounded-md 
+              hover:scale-110 text-white text-base ease-in-out duration-300"
+            >
+              Open
+              <ArrowsPointingOutIcon width={15} className="ml-[10px]" />
+            </a>
+            <img
+              src="/web3d.webp"
               alt="socail"
               className="lg:block hidden object-cover h-full max-w-[370px] rounded-md"
             />
